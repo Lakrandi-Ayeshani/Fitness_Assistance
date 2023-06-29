c3e92d0b18adf202f5cdca466a3f4ea45402957b
Lakrandi-Ayeshani <Lakrandiayeshani1204@gmail.com>
2023-06-29 23:20:42 +0530

    style changes exercise Form

--- front-end/src/Component/ExerciseForm.js
+++ front-end/src/Component/ExerciseForm.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
+import Card from "react-bootstrap/Card"
 import Button from "react-bootstrap/esm/Button";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
@@ -42,33 +43,35 @@ const ExerciseForm = ({ mode, selectedExercise }) => {
     })
 
     return (
-       <Form onSubmit={handleSubmit}> 
-       <h3><b>{mode === "Add" ? "Add Exercise" : "Edit Exercise"}</b></h3>
-        <Row>
-            <Form.Group controlId="formGroupName">
-                <Form.Label>Name</Form.Label>
-                <Form.Control
-                    placeholder="enter exercise name"
-                    onChange={handleChange}
-                    name="name"
-                    value={exerciseData.name}
-                />
-            </Form.Group>
-        </Row>
-        <Row>
-            <Form.Group controlId="formGroupName">
-                <Form.Label>Description</Form.Label>
-                <Form.Control
-                    placeholder="enter exercise description"
-                    onChange={handleChange}
-                    name="description"
-                    value={exerciseData.description}
-                />
-            </Form.Group>
-        </Row>
-            <Button variant="light" type="submit" ><Link to="/" style={{textDecoration:"none"}}>Back Home</Link></Button>
-            <Button type="submit" >Submit</Button>
-       </Form>
+        <Card className="exercise-form-card">
+            <Form onSubmit={handleSubmit}> 
+                <h3><b>{mode === "Add" ? "Add Exercise" : "Edit Exercise"}</b></h3>
+                <Row>
+                    <Form.Group controlId="formGroupName">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control
+                            placeholder="enter exercise name"
+                            onChange={handleChange}
+                            name="name"
+                            value={exerciseData.name}
+                        />
+                    </Form.Group>
+                </Row>
+                <Row>
+                    <Form.Group controlId="formGroupName">
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control
+                            placeholder="enter exercise description"
+                            onChange={handleChange}
+                            name="description"
+                            value={exerciseData.description}
+                        />
+                    </Form.Group>
+                </Row>
+                <Button variant="light" type="submit" ><Link to="/" style={{textDecoration:"none"}}>Back Home</Link></Button>
+                <Button type="submit" >Submit</Button>
+            </Form>
+        </Card>
     )
 }
 
